0c7dbd0f315273112d89821364a7b12d55f0626c
Rob Winch <rwinch@vmware.com>
2013-09-06 11:55:59 -0500

    Fix ClassCastException when using Android client

--- code/web/oauth/src/main/java/com/jl/crm/web/SecurityControllerAdvice.java
+++ code/web/oauth/src/main/java/com/jl/crm/web/SecurityControllerAdvice.java
@@ -1,11 +1,8 @@
 package com.jl.crm.web;
 
-import com.jl.crm.services.*;
-import com.jl.crm.services.security.CrmUserDetailsService;
 import org.springframework.security.core.Authentication;
-import org.springframework.web.bind.annotation.*;
-
-import javax.inject.Inject;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ModelAttribute;
 
 /**
  * Follows good advice demonstrated in <A href="http://stackoverflow.com/questions/17741787/injecting-custom-principal-to-controllers-by-spring-security/17751357#17751357">this
@@ -17,21 +14,12 @@ import javax.inject.Inject;
 @ControllerAdvice
 public class SecurityControllerAdvice {
 
-	private CrmService service;
-
-	@Inject
-	public SecurityControllerAdvice(CrmService service) {
-		this.service = service;
-	}
-
 	@ModelAttribute
-	public User currentUser(Authentication authentication) {
+	public Object currentUser(Authentication authentication) {
 		if (null == authentication){
 			return null;
 		}
-		CrmUserDetailsService.CrmUserDetails crmUserDetails = (CrmUserDetailsService.CrmUserDetails) authentication.getPrincipal();
-		long userId = crmUserDetails.getUser().getId();
-		return this.service.findById(userId);
+		return authentication.getPrincipal();
 	}
 
 }
